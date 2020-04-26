a72a19ebb2bc9c65a5789e2527b296364ddc1bd3
zhuofengwu920 <60891700+zhuofengwu920@users.noreply.github.com>
2020-04-26 10:21:45 +0200

    fixed hyperlink

---
+++ JavaScript/ConfirmToOthersite.js
@@ -0,0 +1,21 @@
+document.body.innerHTML = "hey";
+
+let hyperLink = document.getElementsByClassName("other-site")[0];
+
+hyperLink.addEventListener("click", 
+    function()
+    {
+        /*let goingToOther = confirm("You are about to leave this site to another site. Are you sure?");
+
+        if(goingToOther)
+        {
+
+        }
+        else
+        {
+
+        }*/
+
+        alert("?");
+    }
+);
