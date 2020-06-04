e51efa4faa734479842e49e2b94f1708d0e62e2c
zhuofengwu920 <60891700+zhuofengwu920@users.noreply.github.com>
2020-06-04 09:18:33 +0200

    nav attempt1

--- JavaScript/Button.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementsByClassName("YEET")[0];
-
-button.addEventListener("click", function()
-{
-    alert("YEET");
-}
-);
---
+++ JavaScript/GenerateNav.js
@@ -0,0 +1,28 @@
+const menuItem = [
+    { title: 'Home', link: '/WSOA3028A_1844044/' },
+    { title: 'Blog', link: '/WSOA3028A_1844044/Blog' },
+    { title: 'About', link: '/WSOA3028A_1844044/About' },
+    { title: 'Contact', link: '/WSOA3028A_1844044/Contact' }
+]
+
+function initialiseMenu(){
+    const nav = document.querySelector('nav');
+
+    const ul = document.createElement('ul');
+
+    for (let item of menuItem) 
+    {
+        const li  = document.createElement('li');
+        const a = document.createElement('a');
+        
+        a.innerText = item.title
+        a.href = item.link;
+
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+
+    nav.appendChild(ul);
+}
+
+document.addEventListener("DOMContentLoaded", () => initialiseMenu());
